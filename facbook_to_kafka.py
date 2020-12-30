78b41b11a66f5c2eff976afd0fdb5dbbfe206b8a
Tanmay Solanki <tony@MacBook-Pro-3.lan>
2020-12-30 18:40:17 -0400

    facebook_to_kafka complete

--- facbook_to_kafka.py
+++ facbook_to_kafka.py
@@ -7,7 +7,28 @@ class FacebookStreamProducer():
     ''' Class to read the facbook live stream and push it to Kafka topic '''
 
     def __init__(self):
+        client= KafkaClient('localhost:9092')
+        self.producer=SimpleProducer(client,async=True,
+                                      batch_send_every_n=1000,
+                                      batch_send_every_t=10)
+    
+    def on_message(self,status):
+        # This method will take messages whenever new messages arrives from live comments stream.
+        # here I have aynchronously pushed the messeges to topic called 'facebookstream'
+        message=status
+        try:
+            self.producer.send_messages('facebookstream',message)
+        except Exception as ex:
+            print(ex)
+            return False
+        return True
 
+    def on_error(self,status_code):
+        print('error recived from kafka producer')
+        return True #Dont kill the stream
+    
+    def on_timeout(self):
+        return True #Dont kill the stream
 
 if __name__ =='__main__':
     #to simulate facbook live stream ,we will load comments form a file in a streaming fashion
@@ -16,7 +37,7 @@ if __name__ =='__main__':
     stream=FacebookStreamProducer()
     i=0
     for data in f:
-        stream.on_status(data.strip())
+        stream.on_message(data.strip())
         i+=1
         if i%10000 == 0:
             print('pushed '+i+' elements')
